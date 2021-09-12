afd9de826be174e89f16c59976c0304fd8527056
Savely <fizer_00@mail.ru>
2021-09-12 17:38:48 +0300

    сделал главную стр

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,22 @@
 $(function(){
   
 });
+
+
+new Swiper('.swiper',{
+  loop:true,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  slidesPerView: 1,
+  speed: 2000,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+ });
