faeb674cad1f9e6293ab15150f44deb22d5dd651
DeathWalker47 <fizer_00@mail.ru>
2021-09-18 14:53:46 +0300

    адаптивное меню

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
 $(function(){
- 
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active');
+    $('body').toggleClass('body-active');
+    $('main').toggleClass('main--active');
+  });
 });
 
 
